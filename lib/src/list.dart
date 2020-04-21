576cc505957334da906671ea69861122a84ac113
Jonathan Aird <jonathan.aird@gmail.com>
2020-04-21 16:57:24 +0300

    ...

--- lib/src/list.dart
+++ lib/src/list.dart
@@ -1,15 +1,13 @@
 import 'base.dart';
 import 'dart:collection';
 
-
 //TODO: should override List methods to only notify once for batch changes
 class StateList extends StateElement with ListMixin {
   List<StateElement> _list;
   final StateElement parent;
-  final StatePath path;
 
-  StateList(List list, this.path, this.parent) {
-    _list = toStateElementList(list, path, this);
+  StateList(List list, this.parent) {
+    _list = toStateElementList(list, this);
   }
 
   operator [](int index) => _list[index];
@@ -27,44 +25,39 @@ class StateList extends StateElement with ListMixin {
     _list.length = newLength;
   }
 
-  @override 
-  void addAll(Iterable iterable){
+  @override
+  void addAll(Iterable iterable) {
     iterable.forEach((element) {
-      StatePath newPath = StatePath.from(path);
-      newPath.add(_list.length);
-      _list.add(toStateElement(element,newPath,this));
+      _list.add(toStateElement(element, this));
     });
     notifyChange();
   }
 
-  @override 
-  void clear(){
-    _list.forEach((element) {element.removeFromTree();});
+  @override
+  void clear() {
+    _list.forEach((element) {
+      element.removeFromTree();
+    });
     _list.clear();
     notifyChange();
   }
 
-  @override 
-  void fillRange(int start, int end, [fillValue]){
+  @override
+  void fillRange(int start, int end, [fillValue]) {
     var newFillValue = fillValue;
-    if(fillValue!=null){
-      newFillValue = toStateElement(fillValue, path, this);
+    if (fillValue != null) {
+      newFillValue = toStateElement(fillValue, this);
     }
     _list.fillRange(start, end, newFillValue);
     notifyChange();
   }
-
 }
 
-List<StateElement> toStateElementList(
-    List list, StatePath path, StateElement parent) {
-
+List<StateElement> toStateElementList(List list, StateElement parent) {
   List<StateElement> newList = [];
 
   for (int i = 0; i < list.length; i++) {
-    StatePath newPath = StatePath.from(path);
-    newPath.add(i);
-    newList.add(toStateElement(list[i], newPath, parent));
+    newList.add(toStateElement(list[i],  parent));
   }
 
   return newList;
